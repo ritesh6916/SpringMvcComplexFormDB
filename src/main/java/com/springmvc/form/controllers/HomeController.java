df05ea7c529bb3b334a5017296afcbff37413095
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-02 20:32:57 +0530

    fetch data from, saved files on server and displayed other fields to next page.

--- src/main/java/com/springmvc/form/controllers/HomeController.java
+++ src/main/java/com/springmvc/form/controllers/HomeController.java
@@ -1,20 +1,47 @@
 package com.springmvc.form.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springmvc.form.model.FormModel;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping("/")
-	public String homeController()
-	{
+	public String homeController() {
 		return "FormHome";
 	}
-	
-	@RequestMapping("processForm")
-	public String processForm()
-	{
+
+	@RequestMapping(path = "processForm", method = RequestMethod.POST)
+	public String processForm(@ModelAttribute FormModel formModel, HttpSession s, Model model) {
+		System.out.println(formModel);
+		model.addAttribute(formModel);
+		String imagePath=s.getServletContext().getRealPath("/")+"WEB-INF"+File.separator+"resources"+File.separator + "images"+File.separator+formModel.getImage().getOriginalFilename();
+		String resumePath=s.getServletContext().getRealPath("/")+"WEB-INF"+File.separator+"resources"+File.separator + "images"+File.separator+formModel.getResume().getOriginalFilename();
+		
+		byte[] image=formModel.getImage().getBytes();
+		byte[] resume=formModel.getResume().getBytes();
+		try {
+			FileOutputStream fosImage =new FileOutputStream(imagePath);
+			FileOutputStream fosResume =new FileOutputStream(resumePath);
+			fosImage.write(image);
+			fosResume.write(resume);
+			fosImage.close();
+			fosResume.close();
+		} catch (IOException e) {
+			System.out.println("File Writting Failed");
+		}
 		return "ShowFormData";
 	}
 }
