0012c2d6d881ad65c4a4bec7f4b08411c8a8d0c2
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-04 02:13:35 +0530

    sql-server(mySQL)form data insert done.

--- src/main/java/com/springmvc/form/dao/FormDao.java
+++ src/main/java/com/springmvc/form/dao/FormDao.java
@@ -1,5 +1,25 @@
 package com.springmvc.form.dao;
 
-public class FormDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
+public class FormDao {
+	
+	@Autowired
+	private FormDatabaseTemplate formData;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public int insertFormData(FormDatabaseTemplate data)
+	{
+		formData=data;
+		String query="insert into formdata values(?,?,?,?,?,?,?,?,?,?,?,?)";
+		int rowinserted =jdbcTemplate.update(query,formData.getName(),formData.getPhone(),formData.getGender(),formData.getDob(),
+				formData.getEmail(),formData.getPassword(),formData.getAddress(),formData.getPincode(),
+				formData.getImage(),formData.getCity(),formData.getState(),formData.getResume());
+		return rowinserted;
+	}
+	
 }
