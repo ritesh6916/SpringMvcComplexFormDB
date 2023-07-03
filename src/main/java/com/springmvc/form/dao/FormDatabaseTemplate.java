de30a97e950374a3d2c204046ddf446897c460c8
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-03 23:25:26 +0530

    template complete

--- src/main/java/com/springmvc/form/dao/FormDatabaseTemplate.java
+++ src/main/java/com/springmvc/form/dao/FormDatabaseTemplate.java
@@ -1,5 +1,95 @@
 package com.springmvc.form.dao;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class FormDatabaseTemplate {
 
+	private String name;
+	private String phone;
+	private String gender;
+	private Date dob;
+	private String email;
+	private String password;
+	private String address;
+	private String pincode;
+	private byte[] image;
+	private String city;
+	private String state;
+	private byte[] resume;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public byte[] getResume() {
+		return resume;
+	}
+	public void setResume(byte[] resume) {
+		this.resume = resume;
+	}
+	
 }
